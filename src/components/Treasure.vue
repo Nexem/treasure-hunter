a9e9cba70d51a18d9c892b48ddacfd19db2b9db9
VincentDrn <vincent.dairien@gmail.com>
2023-09-14 11:07:24 +0200

    logic done

--- src/components/Treasure.vue
+++ src/components/Treasure.vue
@@ -2,12 +2,12 @@
  <v-container>
   <v-row class="text-center justify-center align-center">
    <v-col cols="12">
-    <!-- <v-row class="justify-center">
-          <div class="display-2 font-weight-bold mb-3">Welcome to</div>
-        </v-row>
-        <v-row class="justify-center">
-          <div class="display-2 font-weight-bold mb-3">Treasure Hunter</div>
-        </v-row> -->
+    <v-row class="justify-center">
+     <div class="display-2 font-weight-bold mb-3">Carbon IT</div>
+    </v-row>
+    <v-row class="justify-center">
+     <div class="display-2 font-weight-bold mb-3">Treasure Hunter</div>
+    </v-row>
    </v-col>
 
    <v-col class="justify-center align-center">
@@ -15,15 +15,16 @@
    </v-col>
   </v-row>
 
-  <!-- <v-row class="ma-5 justify-center">
-   <v-btn @click="renderResults"> Render </v-btn>
-  </v-row> -->
-
   <v-row>
    <v-textarea v-model="output" outlined></v-textarea>
   </v-row>
   <v-row>
-   <v-textarea v-model="mapDisplay" outlined></v-textarea>
+   <v-textarea v-model="strOut" outlined></v-textarea>
+  </v-row>
+  <v-row>
+   <v-btn v-if="strOut !== ''" @click="downloadResults">
+    Télécharger le résultat
+   </v-btn>
   </v-row>
  </v-container>
 </template>
@@ -43,10 +44,13 @@ export default {
   startPositionAdventurer: [],
 
   listOrientation: ["N", "E", "S", "O"],
-  indexOrientation: 0,
+
+  listAdventurers: [],
 
   positionX: 0,
   positionY: 0,
+
+  strOut: "",
  }),
 
  methods: {
@@ -54,8 +58,10 @@ export default {
    var lines = output.split(/\r?\n/);
    for (var i = 0; i < lines.length; i++) {
     const line = lines[i].replace(/\s+/g, "").split("-").join("");
+    const lineRaw = lines[i];
     // Ajouter une vérif pour éviter un doublon de map qui donnerai une map x2 si deux entrées C
     if (line[0] === "C") {
+     this.strOut += lineRaw;
      for (var row = 0; row < line[2]; row++) {
       this.map.push([]);
       for (var col = 0; col < line[1]; col++) {
@@ -71,10 +77,14 @@ export default {
    var lines = output.split(/\r?\n/);
    for (var i = 0; i < lines.length; i++) {
     const line = lines[i].replace(/\s+/g, "").split("-").join("");
+    const lineRaw = lines[i];
     switch (line[0]) {
      case "#":
       break;
      case "M":
+      this.strOut +=
+       `
+` + lineRaw;
       this.map[line[2]][line[1]] = "M";
       break;
      case "T":
@@ -82,17 +92,17 @@ export default {
       break;
      case "A":
       var adventurer = lines[i].split(" ");
-      this.map[parseInt(adventurer[6])][parseInt(adventurer[4])] = {
-       entity: "A",
-       value: adventurer[2],
+
+      this.map[parseInt(adventurer[6])][parseInt(adventurer[4])] = "A";
+      this.listAdventurers.push({
+       name: adventurer[2],
        orientation: adventurer[8],
+       indexOrientation: this.getIndexOrientation(adventurer[8]),
        move: adventurer[10],
        tresor: 0,
-      };
-      this.startPositionAdventurer.push([
-       parseInt(adventurer[6]),
-       parseInt(adventurer[4]),
-      ]);
+       positionX: parseInt(adventurer[6]),
+       positionY: parseInt(adventurer[4]),
+      });
       break;
     }
    }
@@ -102,6 +112,7 @@ export default {
    this.moveAdventurer();
   },
 
+  // Retrieve the first index orientation of the adventurer
   getIndexOrientation(orientation) {
    switch (orientation) {
     case "N":
@@ -115,78 +126,57 @@ export default {
    }
   },
 
+  // Compute and move the adventurer in the map, getting consideration of orientation and obstacles
   moveAdventurer() {
-   const nbAdventurer = this.startPositionAdventurer.length;
-   for (var adventurer = 0; adventurer < nbAdventurer; adventurer++) {
-    // Get starting position of the adventurer
-    var positionX = this.startPositionAdventurer[adventurer][1];
-    var positionY = this.startPositionAdventurer[adventurer][0];
+   this.listAdventurers.forEach((adventurer) => {
+    const nbactions = adventurer.move.length;
 
-    // Get adventurer saved informations
-    var caracteristics = this.map[positionX][positionY];
-
-    this.indexOrientation = this.getIndexOrientation(
-     caracteristics.orientation
-    );
-
-    this.positionX = positionX;
-    this.positionY = positionY;
-
-    // Nombre d'occurence d'actions à effectuer
-    const nbactions = caracteristics.move.length;
+    this.positionX = adventurer.positionX;
+    this.positionY = adventurer.positionY;
 
     for (var action = 0; action < nbactions; action++) {
-     console.log(this.indexOrientation, " ", caracteristics.orientation);
-     if (caracteristics.move[0] == "A") {
-      this.checkMove(
-       this.positionX,
-       this.positionY,
-       caracteristics.orientation,
+     if (adventurer.move[0] == "A") {
+      adventurer = this.checkMove(
+       adventurer.positionX,
+       adventurer.positionY,
        adventurer
       );
-     } else if (caracteristics.move[0] === "G") {
-      if (this.indexOrientation - 1 > -1) {
-       this.indexOrientation = this.indexOrientation - 1;
+     } else if (adventurer.move[0] === "G") {
+      if (adventurer.indexOrientation - 1 > -1) {
+       adventurer.indexOrientation = adventurer.indexOrientation - 1;
       } else {
-       this.indexOrientation = 3;
+       adventurer.indexOrientation = 3;
       }
-     } else if (caracteristics.move[0] === "D") {
-      if (this.indexOrientation + 1 < 4) {
-       this.indexOrientation = this.indexOrientation + 1;
+     } else if (adventurer.move[0] === "D") {
+      if (adventurer.indexOrientation + 1 < 4) {
+       adventurer.indexOrientation = adventurer.indexOrientation + 1;
       } else {
-       this.indexOrientation = 0;
+       adventurer.indexOrientation = 0;
       }
      }
-     caracteristics.orientation = this.listOrientation[this.indexOrientation];
-     caracteristics.move = caracteristics.move.substring(1);
+     adventurer.orientation = this.listOrientation[adventurer.indexOrientation];
+     adventurer.move = adventurer.move.substring(1);
     }
-    // this.saveEndMap(adventurer);
-    console.log(this.map.length, this.map[1].length);
-    console.log(this.positionX, this.positionY);
-    this.mapDisplay[this.positionX][this.positionY] =
-     this.map[this.startPositionAdventurer[adventurer][0]][
-      this.startPositionAdventurer[adventurer][1]
-     ];
-    this.mapDisplay[this.startPositionAdventurer[adventurer][0]][
-     this.startPositionAdventurer[adventurer][1]
-    ] = ".";
+
+    this.mapDisplay[adventurer.positionX][adventurer.positionY] =
+     this.map[this.positionX][this.positionY];
+    this.mapDisplay[this.positionX][this.positionY] = ".";
     console.log(this.mapDisplay);
-   }
+    this.saveEndMap();
+   });
   },
 
-  checkMove(x, y, orientation, adventurer) {
-   console.log("x :", x, " y :", y);
-   switch (orientation) {
+  // Check if the move in one direction is possible (no montain / adventurer)
+  // and if there is a treasure (will increment players score by 1 from the treasure)
+  checkMove(x, y, adventurer) {
+   switch (adventurer.orientation) {
     case "N":
      x -= 1;
-     if (x > -1 && this.map[x][y] !== "M") {
-      console.log("ok");
-      this.positionX = this.positionX - 1;
+     if (x > -1 && this.map[x][y] !== ("M" || "A")) {
+      adventurer.positionX = adventurer.positionX - 1;
       if (this.map[x][y].entity && this.map[x][y].entity === "T") {
        this.map[x][y].value -= 1;
-       this.map[this.startPositionAdventurer[adventurer][0]][
-        this.startPositionAdventurer[adventurer][1]
-       ].tresor += 1;
+       adventurer.tresor += 1;
        if (this.map[x][y].value === 0) {
         this.map[x][y] === ".";
        }
@@ -197,14 +187,11 @@ export default {
      break;
     case "E":
      y += 1;
-     if (y < this.map[0].length && this.map[x][y] !== "M") {
-      console.log("ok", this.map[x][y]);
-      this.positionY = this.positionY + 1;
+     if (y < this.map[0].length && this.map[x][y] !== ("M" || "A")) {
+      adventurer.positionY = adventurer.positionY + 1;
       if (this.map[x][y].entity && this.map[x][y].entity === "T") {
        this.map[x][y].value -= 1;
-       this.map[this.startPositionAdventurer[adventurer][0]][
-        this.startPositionAdventurer[adventurer][1]
-       ].tresor += 1;
+       adventurer.tresor += 1;
        if (this.map[x][y].value === 0) {
         this.map[x][y] === ".";
        }
@@ -215,14 +202,11 @@ export default {
      break;
     case "S":
      x += 1;
-     if (x < this.map.length && this.map[x][y] !== "M") {
-      console.log("ok", this.map[x][y]);
-      this.positionX = this.positionX + 1;
+     if (x < this.map.length && this.map[x][y] !== ("M" || "A")) {
+      adventurer.positionX = adventurer.positionX + 1;
       if (this.map[x][y].entity && this.map[x][y].entity === "T") {
        this.map[x][y].value -= 1;
-       this.map[this.startPositionAdventurer[adventurer][0]][
-        this.startPositionAdventurer[adventurer][1]
-       ].tresor += 1;
+       adventurer.tresor += 1;
        if (this.map[x][y].value === 0) {
         this.map[x][y] === ".";
        }
@@ -233,14 +217,11 @@ export default {
      break;
     case "O":
      y -= 1;
-     if (y > -1 && this.map[x][y] !== "M") {
-      console.log("ok", this.map[x][y]);
-      this.positionY = this.positionY - 1;
+     if (y > -1 && this.map[x][y] !== ("M" || "A")) {
+      adventurer.positionY = adventurer.positionY - 1;
       if (this.map[x][y].entity && this.map[x][y].entity === "T") {
        this.map[x][y].value -= 1;
-       this.map[this.startPositionAdventurer[adventurer][0]][
-        this.startPositionAdventurer[adventurer][1]
-       ].tresor += 1;
+       adventurer.tresor += 1;
        if (this.map[x][y].value === 0) {
         this.map[x][y] === ".";
        }
@@ -250,20 +231,51 @@ export default {
      }
      break;
    }
+   return adventurer;
   },
 
-  saveEndMap(adventurer) {
-   console.log(this.positionX);
-   console.log(this.positionY);
-   this.mapDisplay[this.positionX][this.positionY] =
-    this.map[this.startPositionAdventurer[adventurer][0]][
-     this.startPositionAdventurer[adventurer][1]
-    ];
-   this.mapDisplay[this.startPositionAdventurer[adventurer][0]][
-    this.startPositionAdventurer[adventurer][1]
-   ] = ".";
+  // Create final result, displayed on screen
+  // It can also be downloaded
+  saveEndMap() {
+   for (var row = 0; row < this.mapDisplay.length; row++) {
+    for (var col = 0; col < this.mapDisplay[0].length; col++) {
+     if (
+      this.mapDisplay[row][col].entity &&
+      this.mapDisplay[row][col].entity === "T"
+     ) {
+      this.strOut +=
+       `
+` +
+       "T - " +
+       col +
+       " - " +
+       row +
+       " - " +
+       this.mapDisplay[row][col].value;
+     }
+    }
+   }
+   this.listAdventurers.forEach((adventurer) => {
+    var result =
+     "A - " +
+     adventurer.name +
+     " - " +
+     adventurer.positionX +
+     " - " +
+     adventurer.positionY +
+     " - " +
+     adventurer.orientation +
+     " - " +
+     adventurer.tresor;
+
+    //  Push result in the end string display
+    this.strOut +=
+     `
+` + result;
+   });
   },
 
+  // Read entry file and store it as a string
   readFile() {
    this.file = this.$refs.doc.files[0];
    const reader = new FileReader();
@@ -282,6 +294,15 @@ export default {
     reader.onerror = (err) => console.log(err);
     reader.readAsText(this.file);
    }
+  },
+
+  // Create output result file from the strOut string
+  downloadResults() {
+   var link = document.createElement("a");
+   link.download = "resultsTreasure.txt";
+   var blob = new Blob([this.strOut], { type: "text/plain" });
+   link.href = window.URL.createObjectURL(blob);
+   link.click();
   },
  },
 };
